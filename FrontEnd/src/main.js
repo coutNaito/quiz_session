1e0eb5ee5f7f0f94ae75ed85b1f64e026ad307c6
Ekaruch Purathananont <73429510+coutNaito@users.noreply.github.com>
2024-04-17 16:28:07 +0700

    Structure folder

---
+++ FrontEnd/src/main.js
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+//websocket
+// import { Vue } from 'vue';
+// import socketio from 'socket.io';
+// import VueSocketIO from 'vue-sock?et.io';
+
+// export const SocketInstance = socketio('http://localhost:4113');
+
+// Vue.use(VueSocketIO, SocketInstance)
+
+
+
+createApp(App).mount('#app')
